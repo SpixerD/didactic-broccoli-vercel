244255dfa8a2e39b36909ecac976b363242f364f
Mohammed Barrouch <120631086+SpixerD@users.noreply.github.com>
2025-09-29 15:17:42 +0100

    Update licenses.js

--- api/admin/licenses.js
+++ api/admin/licenses.js
@@ -1,6 +1,6 @@
-import LicenseDB from '../../lib/db';
+const LicenseDB = require('../lib/db');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Handle CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
